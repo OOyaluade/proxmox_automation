fdf0a5e9e2e429f90afb8cff5ad045b06e9c84c4
root <oyaluadedamilola@damilstudio.com>
2025-05-31 16:09:14 -0500

    quick save

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,13 +1,21 @@
-# module "vyos_router" {
-#   source    = "./modules/vyos"
-#   providers = {
-#     proxmox = proxmox
-#   }
-# }
+module "vyos_router" {
+  source    = "./modules/vyos"
+  providers = {
+    proxmox = proxmox
+  }
+}
 
-# module "esxi" {
-#   source = "./modules/esxi"
-#   providers = {
-#     proxmox = proxmox
-#   }
-# }
+module "esxi" {
+  source = "./modules/esxi"
+  providers = {
+    proxmox = proxmox
+  }
+}
+
+module "ontap" {
+  source = "./modules/ontap"
+  providers = {
+    proxmox = proxmox
+  }
+}
+
